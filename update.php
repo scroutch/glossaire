1b48b7918376c7c8cd481154b30209911b11d866
scroutch <cecile.duhain@gmail.com>
2022-08-31 12:57:19 +0200

    update finish

--- update.php
+++ update.php
@@ -7,15 +7,15 @@ if(((isset($_POST['word'])) && $_POST['word'] != null) &&
     $id = strip_tags($_GET['id']);
     $word = strip_tags($_POST['word']);
     $def = strip_tags($_POST['def']);
-    // $firstLetter = substr($word, 0 , 1);
-    // $firstLetter = strtoupper($firstLetter);
+    $firstLetter = substr($word, 0 , 1);
+    $firstLetter = strtoupper($firstLetter);
 
-    $query = 'UPDATE definition SET word=:word, `definition`=:def WHERE id=:id';
+    $query = 'UPDATE definition SET word=:word, `definition`=:def, first_letter=:first_letter WHERE id=:id';
     $req = $bdd -> prepare($query);
     $req -> bindValue(':word', $word, PDO::PARAM_STR);
     $req -> bindValue(':def', $def, PDO::PARAM_STR);
     $req->bindValue(':id', $id, PDO::PARAM_INT);
-    // $req -> bindValue(':first_letter', $firstLetter, PDO::PARAM_STR);
+    $req -> bindValue(':first_letter', $firstLetter, PDO::PARAM_STR);
     $req -> execute();
 
     header('Location: ./index.php?page=5');
